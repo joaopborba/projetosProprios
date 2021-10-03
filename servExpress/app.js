d68b22ad1a1ce3b21c7f78de3dfa0ec3957f7fa4
João Pedro <joaopborba95@hotmail.com>
2021-10-03 20:49:35 -0300

    Acrescentando rotas parametrizadas

--- servExpress/app.js
+++ servExpress/app.js
@@ -13,6 +13,16 @@ app.get("/contato", (req,res)=>{
     res.send({nome:"João Pedro", idade: 25, profissao: "Developer"});
 })
 
+app.get("/produtos/:id", (req,res)=>{
+    let {id} = req.params;
+    res.send("O produto pesquisado é o id: " + id);
+})
+
+app.get("/perfis/:id?", (req,res)=>{
+    let{id} = req.params;
+    res.send("O id procurado é: " + id);
+})
+
 app.listen(3000, ()=>{
     console.log("Servidor rodando na porta 3000");
 })
