68c6377fc4aa3d8ba3478d49d1cf1f91e76e8628
João Pedro <joaopborba95@hotmail.com>
2021-10-04 19:15:26 -0300

    Modificando e usando Tags EJS

--- projetoExpGenerator/routes/index.js
+++ projetoExpGenerator/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const contatoController = require('../controllers/contatoController');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/contato", contatoController.index);
 
 module.exports = router;
