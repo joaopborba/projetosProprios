66700c59b5160f0480a26a0fed65e499d766975e
João Pedro <joaopborba95@hotmail.com>
2021-10-09 22:41:55 -0300

    Acrescentando PUT e DELETE

--- projetoExpGenerator/controllers/produtoController.js
+++ projetoExpGenerator/controllers/produtoController.js
@@ -8,6 +8,30 @@ const produtoController = {
     },
     sucesso: (req, res)=>{
         res.render('sucesso');
+    },
+    viewAttForm: (req, res)=>{
+        let {id} = req.params;
+        let produtos = [
+            {id:1, nome: "Produto X", preco:10},
+            {id:2, nome: "Produto Y", preco:20}
+        ]
+        res.render('editarProduto', {produto:produtos[id]});
+    },
+    editar: (req, res)=>{
+        let { nomeProduto, precoProduto } = req.body;
+        res.send('Você editou o ' + nomeProduto);
+    },
+    listarProdutos: (req, res)=>{
+        let produtos = [
+            {id:1, nome: "Produto X", preco:10},
+            {id:2, nome: "Produto Y", preco:20}
+        ]
+        res.render('listaProdutos', {listaProdutos:produtos});
+    },
+    deletarProduto: (req, res)=>{
+        let {id} = req.params;
+
+        res.send("Deletando o produto com id: " + id);
     }
 }
 
--- projetoExpGenerator/routes/produtos.js
+++ projetoExpGenerator/routes/produtos.js
@@ -6,6 +6,10 @@ const produtoController = require('../controllers/produtoController');
 router.get('/criar', produtoController.viewForm);
 router.post('/criar', produtoController.salvarForm);
 router.get('/sucesso', produtoController.sucesso);
+router.get('/:id/editar', produtoController.viewAttForm);
+router.put('/editar', produtoController.editar);
+router.get('/', produtoController.listarProdutos);
+router.delete('/deletar/:id', produtoController.deletarProduto);
 
 
 module.exports = router;
